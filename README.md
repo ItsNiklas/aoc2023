4a47bba02cace4e7a2649c875d8c67067e1f7123
ItsNiklas <nikbau2000@gmail.com>
2023-12-17 09:04:35 +0100

    Day 17

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ My solutions for [Advent of Code](https://adventofcode.com/2023) in 2023. Probab
 
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  2023 - 32 ⭐
+  2023 - 34 ⭐
 </h1>
 <a href="solutions/day01/day01.cpp">
   <img src="Media/2023/01.png" width="161px">
@@ -56,7 +56,7 @@ My solutions for [Advent of Code](https://adventofcode.com/2023) in 2023. Probab
 <a href="solutions/day16/day16.py">
   <img src="Media/2023/16.png" width="161px">
 </a>
-<a href="None">
+<a href="solutions/day17/day17.py">
   <img src="Media/2023/17.png" width="161px">
 </a>
 <a href="None">
---
+++ solutions/day17/day17.py
@@ -0,0 +1,54 @@
+from heapq import *
+
+inp = open(0).read().splitlines()
+N, M = len(inp), len(inp[0])
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+
+def dj(ultra=False):
+    d, q = {}, []
+    heappush(q, (0, 0, 0, -1, -1))
+
+    while q:
+        dist, y, x, dir, steps = heappop(q)
+
+        if (y, x, dir, steps) in d:
+            continue
+
+        d[(y, x, dir, steps)] = dist
+        
+        for i in range(4):
+            vy, vx = y + dy[i], x + dx[i]
+
+            if not (0 <= vy < N) or not (0 <= vx < M):
+                continue  # Out of bounds
+
+            if ultra and i != dir and steps < 4 and dir != -1:
+                continue  # Can I turn?
+
+            if dir == (i + 2) % 4:
+                continue  # No backsies
+
+            if (new_steps := 1 if i != dir else steps + 1) > (10 if ultra else 3):
+                continue  # Not again
+
+            heappush(q, (dist + int(inp[vy][vx]), vy, vx, i, new_steps))
+
+    return min(
+        v
+        for (y, x, _, steps), v in d.items()
+        if y == N - 1 and x == M - 1 and (not ultra or steps >= 4)
+    )
+
+
+def part1():
+    return dj()
+
+
+def part2():
+    return dj(True)
+
+
+print(part1(), part2())
